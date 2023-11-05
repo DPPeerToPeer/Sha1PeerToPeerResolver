29f16100b820569b053c8c78d232184e0d5aa028
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-11-05 18:21:54 +0100

    add DI in network module

---
+++ network/src/main/java/com/example/network/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.example.network.di
+
+import com.example.network.IConnectionsHandler
+import com.example.network.internal.ConnectionsHandler
+import com.example.network.useCases.ListenNodeMessagesUseCase
+import com.example.network.useCases.SendNodeMessageUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import org.kodein.di.DI
+import org.kodein.di.bindProvider
+import org.kodein.di.bindSingleton
+import org.kodein.di.instance
+
+val networkModule = DI.Module(name = "Network") {
+    bindSingleton<IConnectionsHandler> {
+        ConnectionsHandler(
+            scope = CoroutineScope(SupervisorJob()),
+        )
+    }
+    bindProvider<ListenNodeMessagesUseCase> {
+        ListenNodeMessagesUseCase(instance())
+    }
+    bindProvider<SendNodeMessageUseCase> {
+        SendNodeMessageUseCase(instance())
+    }
+}
