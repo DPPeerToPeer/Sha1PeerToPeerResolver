538d879e8414c8b979d2d6776f6ae79a445cbfe0
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-11-05 18:23:55 +0100

    Start adding test dependencies

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -15,4 +15,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.ktor:ktor-network:2.3.5")
     implementation("org.kodein.di:kodein-di:7.19.0")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("app.cash.turbine:turbine:1.0.0")
 }
