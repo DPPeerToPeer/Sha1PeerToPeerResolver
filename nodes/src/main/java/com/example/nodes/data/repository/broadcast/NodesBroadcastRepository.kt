c46215b0a295a4f24ee79df5de756dac38bbde5e
Krystian <krystiano1602@gmail.com>
2023-12-05 19:14:39 +0100

    sendStartedCalculation method done

--- nodes/src/main/java/com/example/nodes/data/repository/broadcast/NodesBroadcastRepository.kt
+++ nodes/src/main/java/com/example/nodes/data/repository/broadcast/NodesBroadcastRepository.kt
@@ -3,23 +3,35 @@ package com.example.nodes.data.repository.broadcast
 import com.example.common.models.Batch
 import com.example.common.models.CalculationResult
 import com.example.network.ISendNodeMessageUseCase
+import com.example.network.models.NodeMessage
+import com.example.nodes.data.repository.info.INodesInfoRepository
+import com.example.nodes.data.repository.info.NodesInfoRepository
 
 class NodesBroadcastRepository(
-    private val sendNodeMessageUseCase: ISendNodeMessageUseCase
+    private val sendNodeMessageUseCase: ISendNodeMessageUseCase,
+    private val NodesInfoRepository: INodesInfoRepository
 ): INodesBroadcastRepository {
 
+    // 1. Pobieram listę aktywnych nodów. 2. Dla każdego node zawołać metodę invoke z SendNodeMessageUseCase.
     override suspend fun sendMyInfo(port: Int) {
-        TODO("Not yet implemented") // Tego jeszcze nie robic
+        TODO("Yet not implemented")// Tego jeszcze nie robic
+
+
     }
 
     override suspend fun sendStart() {
-        TODO("Not yet implemented")
+        TODO("Not yet implemented")// Tego jeszcze nie robic
+
     }
 
-    override suspend fun sendStartedCalculation(batch: Batch, timestamp: Long) { // dla każdej metody przeiterować po liśćie aktywnych node
-        // wywołać invoke dla każdego node z odpowiednią instancją message
-        TODO("Not yet implemented")
-//        sendNodeMessageUseCase.invoke()
+    override suspend fun sendStartedCalculation(batch: Batch, timestamp: Long) {
+        // Invoke method from sendNodeMessageUseCase for every node sending NodeMessage.StartedCalculation message
+        var message = NodeMessage.StartedCalculation(batch, timestamp)
+        var avtiveNodesList = NodesInfoRepository.getActiveNodes()
+
+        for (node in avtiveNodesList){
+            sendNodeMessageUseCase.invoke(node.id,message)
+        }
     }
 
     override suspend fun sendEndedCalculation(batch: Batch, calculationResult: CalculationResult) {
