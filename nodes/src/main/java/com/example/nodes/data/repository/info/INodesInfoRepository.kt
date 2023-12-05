0d25a5634c394ce5c672eda8899ec93f1567f0b2
Krystian <krystiano1602@gmail.com>
2023-12-05 18:17:04 +0100

    Removing ToDo's

--- nodes/src/main/java/com/example/nodes/data/repository/info/INodesInfoRepository.kt
+++ nodes/src/main/java/com/example/nodes/data/repository/info/INodesInfoRepository.kt
@@ -7,16 +7,16 @@ interface INodesInfoRepository {
 
     //przechowuje listę wszystkich nodów
 
-    suspend fun upsertManyNodes(nodes: List<Node>)//TODO jeśli jest takie same id podmienic. Jeśli nie ma takiego id - dodać.
+    suspend fun upsertManyNodes(nodes: List<Node>)// jeśli jest takie same id podmienic. Jeśli nie ma takiego id - dodać.
 
-    suspend fun getActiveNodes(): List<Node>//TODO Zwracaca listę nodów
+    suspend fun getActiveNodes(): List<Node>// Zwracaca listę nodów
 
-    suspend fun upsertNode(node: Node)//TODO Dodaje pojedyńczy node
+    suspend fun upsertNode(node: Node)// Dodaje pojedyńczy node
 
-    suspend fun removeNode(id: NodeId)//TODO Usuwa pojedyńczy node
+    suspend fun removeNode(id: NodeId)// Usuwa pojedyńczy node
 
     suspend fun updateHealth(
         nodeId: NodeId,
         timestamp: Long,
-    )//TODO // Dla danego node w kolekcji zamienia timestamp na podany jako argument
+    ) // Dla danego node w kolekcji zamienia timestamp na podany jako argument
 }
