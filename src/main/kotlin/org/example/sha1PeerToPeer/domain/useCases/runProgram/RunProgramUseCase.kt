9b59487a0200e1ab69a39b5514043351ebf472ce
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-12-15 18:27:11 +0100

    Extract repetitive operations from RunProgramUseCase to different use case

--- src/main/kotlin/org/example/sha1PeerToPeer/domain/useCases/runProgram/RunProgramUseCase.kt
+++ src/main/kotlin/org/example/sha1PeerToPeer/domain/useCases/runProgram/RunProgramUseCase.kt
@@ -1,34 +1,20 @@
 package org.example.sha1PeerToPeer.domain.useCases.runProgram
 
-import com.example.calculation.ICalculationRepository
-import com.example.common.*
-import com.example.network.IDiscoveryUseCase
 import com.example.network.IRunConnectionsHandlerUseCase
-import com.example.network.models.Port
-import com.example.nodes.INodesBroadcastUseCase
-import com.example.nodes.IRemoveNotActiveNodesUseCase
-import com.example.nodes.ISendHealthUseCase
-import com.example.nodes.data.repository.info.INodesInfoRepository
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import org.example.sha1PeerToPeer.domain.useCases.HandleIncomingNodeMessagesUseCase
+import org.example.sha1PeerToPeer.domain.useCases.RunRepetitiveOperationsUseCase
 
 private val logger = KotlinLogging.logger {}
 
 internal class RunProgramUseCase(
     private val runConnectionsHandlerUseCase: IRunConnectionsHandlerUseCase,
-    private val discoveryUseCase: IDiscoveryUseCase,
-    private val calculationRepository: ICalculationRepository,
-    private val handleIncomingNodeMessagesUseCase: HandleIncomingNodeMessagesUseCase,
-    private val sendHealthUseCase: ISendHealthUseCase,
-    private val removeNotActiveNodesUseCase: IRemoveNotActiveNodesUseCase,
-    private val nodesRepository: INodesInfoRepository,
-    private val nodesBroadcastUseCase: INodesBroadcastUseCase,
-    private val getMyIdUseCase: IGetMyIdUseCase,
     private val appScope: CoroutineScope,
-    private val getCurrentTimeUseCase: IGetCurrentTimeUseCase,
+    private val runRepetitiveOperationsUseCase: RunRepetitiveOperationsUseCase,
 ) : IRunProgramUseCase {
 
     private var job: Job? = null
@@ -48,67 +34,10 @@ internal class RunProgramUseCase(
                 val myPort = runConnectionsHandlerUseCase.invoke()
 
                 job = appScope.launch {
-                    makeRepetitiveOperations(
+                    runRepetitiveOperationsUseCase(
                         hashToFind = hashToFind,
                         myPort = myPort,
                     )
-                }
-            }
-        }
-    }
-
-    private suspend fun makeRepetitiveOperations(
-        hashToFind: String,
-        myPort: Port,
-    ) {
-        coroutineScope {
-            logger.debug {
-                "Repetitive operations start"
-            }
-            launch {
-                discoveryUseCase.invoke(
-                    hashToFind = hashToFind,
-                    myPort = myPort.port,
-                    myId = getMyIdUseCase(),
-                    myName = "Name",
-                ).collect {
-                    nodesRepository.upsertNode(node = it)
-                }
-            }
-
-            delay(AFTER_DISCOVERY_PROGRAM_DELAY) // Give time for discovery for some nodes and start
-
-            launch {
-                handleIncomingNodeMessagesUseCase()
-            }
-            launch {
-                while (isActive) {
-                    val batch = calculationRepository.getAvailableBatchAndMarkMine()
-                    batch?.let {
-                        val job = launch {
-                            nodesBroadcastUseCase.sendStartedCalculation(batch = batch, timestamp = getCurrentTimeUseCase())
-                            calculationRepository.startCalculation(batch = batch)
-                        }
-                        while (!job.isCompleted) {
-                            ensureActive()
-                            if (calculationRepository.isBatchTakenByOtherNode(batch = batch)) {
-                                job.cancel()
-                                break
-                            }
-                        }
-                    } ?: delay(TRY_AGAIN_DELAY_IF_NO_BATCH_AVAILABLE)
-                }
-            }
-            launch {
-                while (isActive) {
-                    sendHealthUseCase()
-                    delay(SEND_HEALTH_INTERVAL)
-                }
-            }
-            launch {
-                while (isActive) {
-                    removeNotActiveNodesUseCase()
-                    delay(REMOVE_NODE_AFTER_INACTIVITY_DURATION)
                 }
             }
         }
