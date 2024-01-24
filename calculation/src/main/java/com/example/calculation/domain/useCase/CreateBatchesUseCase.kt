ceaca01651753ec6349a96e0d07297d5c800c9b0
kSuroweczka <92479853+kSuroweczka@users.noreply.github.com>
2024-01-24 22:56:10 +0100

    Make Activity Diagram

--- calculation/src/main/java/com/example/calculation/domain/useCase/CreateBatchesUseCase.kt
+++ calculation/src/main/java/com/example/calculation/domain/useCase/CreateBatchesUseCase.kt
@@ -10,68 +10,65 @@ internal class CreateBatchesUseCase(
     private val getAvailableCharsUseCase: GetAvailableCharsUseCase,
     private val scope: CoroutineScope,
 ) {
-
     @OptIn(ExperimentalCoroutinesApi::class)
-    operator fun invoke(): ReceiveChannel<Batch> = scope.produce {
-        val batchSize = 5
-        val znaki = getAvailableCharsUseCase()
+    operator fun invoke(): ReceiveChannel<Batch> =
+        scope.produce {
+            val batchSize = 5
+            val znaki = getAvailableCharsUseCase()
 
-        suspend fun iterateLastNElementsForGenerating(
-            start: String,
-            ktoraLiteraOdKonca: Int,
-            end: String,
-        ) {
-            val newElem = start.substring(0, start.length - ktoraLiteraOdKonca)
-            val indeksNastepnegoZnaku =
-                if (start.length - ktoraLiteraOdKonca != start.length) {
-                    znaki.indexOf(start[start.length - ktoraLiteraOdKonca])
+            suspend fun iterateLastNElementsForGenerating(
+                start: String,
+                ktoraLiteraOdKonca: Int,
+                end: String,
+            ) {
+                val newElem = start.substring(0, start.length - ktoraLiteraOdKonca)
+                val indeksNastepnegoZnaku =
+                    if (start.length - ktoraLiteraOdKonca != start.length) {
+                        znaki.indexOf(start[start.length - ktoraLiteraOdKonca])
+                    } else {
+                        znaki.indexOf(start[0])
+                    }
+
+                for (i in znaki.subList(indeksNastepnegoZnaku, znaki.size)) {
+                    var text = newElem + i
+                    if (ktoraLiteraOdKonca >= 2) {
+                        val remainingChars = start.substring(start.length - ktoraLiteraOdKonca + 1)
+                        text += remainingChars
+                    }
+
+                    send(
+                        Batch(
+                            start = text + "aaaa",
+                            end = text + "9999",
+                        ),
+                    )
+
+                    if (text == end) {
+                        println("KONIEC ----- limit wyczerpany!!!")
+                    }
+
+                    if (ktoraLiteraOdKonca - 1 > 0) {
+                        iterateLastNElementsForGenerating(text, ktoraLiteraOdKonca - 1, end)
+                    }
+                }
+            }
+
+            for (i in batchSize until 11) {
+                if (i == batchSize) {
+                    println("POCZĄTEK: a     KONIEC: " + "9".repeat(batchSize - 1))
+                    send(
+                        Batch(
+                            start = "a",
+                            end = "9".repeat(batchSize - 1),
+                        ),
+                    )
                 } else {
-                    znaki.indexOf(start[0])
-                }
-
-            for (i in znaki.subList(indeksNastepnegoZnaku, znaki.size)) {
-                var text = newElem + i
-                if (ktoraLiteraOdKonca >= 2) {
-                    val remainingChars = start.substring(start.length - ktoraLiteraOdKonca + 1)
-                    //                    newElem += remainingChars
-                    text += remainingChars
-                }
-
-                send(
-                    Batch(
-                        start = text + "aaaa",
-                        end = text + "9999",
-                    ),
-                )
-                // println(Pair(text + "aaaa", text + "9999"))
-
-                if (text == end) {
-                    println("KONIEC ----- limit wyczerpany!!!")
-                }
-
-                if (ktoraLiteraOdKonca - 1 > 0) {
-                    iterateLastNElementsForGenerating(text, ktoraLiteraOdKonca - 1, end)
+                    iterateLastNElementsForGenerating(
+                        "a".repeat(i - batchSize),
+                        i - batchSize,
+                        "9".repeat(i - batchSize),
+                    )
                 }
             }
         }
-
-        for (i in batchSize until 11) {
-            if (i == batchSize) {
-                println("POCZĄTEK: a     KONIEC: " + "9".repeat(batchSize - 1))
-                send(
-                    Batch(
-                        start = "a",
-                        end = "9".repeat(batchSize - 1),
-                    ),
-                )
-                // println(Pair("a", "9".repeat(batchSize - 1)))
-            } else {
-                iterateLastNElementsForGenerating(
-                    "a".repeat(i - batchSize),
-                    i - batchSize,
-                    "9".repeat(i - batchSize),
-                )
-            }
-        }
-    }
 }
